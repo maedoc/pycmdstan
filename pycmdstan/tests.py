1647616e9c7150991d3c9f585f76c31e17ef8859
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2018-05-30 16:16:08 +0200

    first tests

---
+++ pycmdstan/tests.py
@@ -0,0 +1,33 @@
+import os
+import unittest
+import tempfile
+import numpy as np
+import numpy.testing
+from . import io
+
+
+class BaseTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        self.tmp_dir = tempfile.TemporaryDirectory()
+        
+    def tearDown(self):
+        self.tmp_dir.cleanup()
+        
+    def tmp_fname(self, fname):
+        return os.path.join(self.tmp_dir.name, fname)
+    
+
+class SimpleIOTests(BaseTestCase):
+    
+    def test_write_read(self):
+        data = {
+            'int': 3,
+            'real': 2.3
+        }
+        fname = self.tmp_fname('write_read.R')
+        io.rdump(fname, data)
+        data_ = io.rload(fname)
+        for key, val in data.items():
+            val_ = data_[key]
+            self.assertEqual(val, val_)
