1fce7ed0ec85eb47b5fed196969c315260309750
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2018-06-05 16:06:27 +0200

    fix pypi push

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ Topic :: Scientific/Engineering :: Information Analysis
 
 setuptools.setup(
     name='pycmdstan',
-    version='0.2.2',
+    version='0.3',
     description='Python interface to CmdStan',
     long_description=readme_contents(),
     long_description_content_type="text/markdown",
