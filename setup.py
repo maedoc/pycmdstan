0cc912bb50700cb4638e1b1099ae28e6ca3a8f39
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2018-06-05 18:15:32 +0200

    bump for testing pypi

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ Topic :: Scientific/Engineering :: Information Analysis
 
 setuptools.setup(
     name='pycmdstan',
-    version='0.3',
+    version='0.4.1',
     description='Python interface to CmdStan',
     long_description=readme_contents(),
     long_description_content_type="text/markdown",
