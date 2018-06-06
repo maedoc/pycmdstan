a57e8de628220220faf7145985f1d89592a4085a
marmaduke <marmaduke.woodman@univ-amu.fr>
2018-06-06 20:07:32 +0000

    avoid _tkinter error on rtd build

--- pycmdstan/__init__.py
+++ pycmdstan/__init__.py
@@ -1,3 +1,11 @@
+# avoid _tkinter import error on readthedocs build
+import os
+if 'READTHEDOCS' in os.environ:
+    import matplotlib
+    matplotlib.use('agg')
+    del matplotlib
+del os
+
 from .io import *
 from .model import *
 from .viz import *
