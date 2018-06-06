f28dd20e72850afd3e79da9f3890389da9508f62
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2018-06-06 10:32:15 +0200

    bump v

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ Topic :: Scientific/Engineering :: Information Analysis
 
 setuptools.setup(
     name='pycmdstan',
-    version='0.4.1',
+    version='0.5',
     description='Python interface to CmdStan',
     long_description=readme_contents(),
     long_description_content_type="text/markdown",
